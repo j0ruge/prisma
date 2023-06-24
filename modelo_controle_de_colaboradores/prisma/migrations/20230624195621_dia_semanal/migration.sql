03c6eea4df52426aecaca984d7ee30f94b272f00
j0ruge <joruge@gmail.com>
2023-06-24 16:57:51 -0300

    ajuste de migração

---
+++ modelo_controle_de_colaboradores/prisma/migrations/20230624195621_dia_semanal/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `dia_semana_culto` on the `cultos` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "cultos" DROP COLUMN "dia_semana_culto";
